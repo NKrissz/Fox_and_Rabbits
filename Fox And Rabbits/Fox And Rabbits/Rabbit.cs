9b20ce1d0669933efb9ab426b96f9a8f966c05dc
rakybalint <145117716+rakybalint@users.noreply.github.com>
2023-09-22 12:58:58 +0200

    Add files via upload

---
+++ Fox And Rabbits/Fox And Rabbits/Rabbit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fox_And_Rabbits
+{
+    internal class Rabbit : Animals
+    {
+      
+
+
+    }
+}
