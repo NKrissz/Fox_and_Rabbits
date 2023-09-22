d8a95a7079409f31e15760f1db45663fa1c2c0c5
rakybalint <145117716+rakybalint@users.noreply.github.com>
2023-09-22 13:31:37 +0200

    Add files via upload

---
+++ Fox And Rabbits/Fox And Rabbits/Simulation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fox_And_Rabbits
+{
+    internal class Simulation
+    {
+
+    }
+}
