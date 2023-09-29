70888d50db5cb11124500758058acee68ef72cfe
price4 <odorics.aron@gmail.com>
2023-09-29 12:49:13 +0200

    Grid change Rabbits/obj/Debug/net7.0-windows/Fox And Rabbits.GeneratedMSBuildEditorConfig.editorconfig

--- Fox And Rabbits/Fox And Rabbits/Form1.cs
+++ Fox And Rabbits/Fox And Rabbits/Form1.cs
@@ -2,9 +2,34 @@ namespace Fox_And_Rabbits
 {
     public partial class Form1 : Form
     {
+        private const int gridSize = 10;
+        private const int tileSize = 50; // Size of each tile in pixels
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid();
+        }
+
+        private void InitializeGrid()
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    var tile = new Panel
+                    {
+                        Size = new Size(tileSize, tileSize),
+                        Location = new Point(x * tileSize, y * tileSize),
+                        BackColor = (x + y) % 2 == 0 ? Color.White : Color.Black
+                    };
+
+                    Controls.Add(tile);
+                }
+            }
+        }
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
         }
     }
-}
